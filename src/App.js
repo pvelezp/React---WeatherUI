8690df981e4f17bf36585af69acca7565962461c
Pablo Velez <pablo.velez.p@gmail.com>
2020-07-11 17:49:22 -0500

    iniciar proyecto y desarrollo de formulario con useState

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+<>
+<Header 
+titulo='Clima react app'
+/>
+
+  <div className="contenedor-form">
+    <div className="container">
+      <div className="row">
+        <div className="col m6 s12">
+         <Formulario /> 
+        </div>
+        <div className="col m6 s12">
+          2
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario.js
@@ -0,0 +1,59 @@
+import React, { useState} from 'react'
+
+const Formulario = () => {
+
+    const [busqueda, guardarBusqueda] =  useState({
+        ciudad: '',
+        pais: ''
+    })
+    // extraer ciudad y pais
+    const {ciudad, pais} = busqueda
+
+        //function que coloca los elementos en el state
+    const handleChange = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+
+    return (
+        <form >
+                    <div className="input-field col s12">
+
+                        <input
+                         type="text"
+                         name="ciudad"
+                         id="ciudad"
+                         value={ciudad}
+                         onChange= {handleChange}
+                         />
+                         <label htmlFor="ciudad" > Ciudad: </label>
+                    </div>
+
+                    <div className="input-field col s12">
+
+                        <select 
+                        name="pais"
+                         id="pais"
+                         value={pais}
+                         onChange= {handleChange}
+                         >
+                         <option value="">-- Seleccione un país --</option>    
+                         <option value="US">Estados Unidos</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="ES">España</option>
+                        <option value="PE">Perú</option>
+                         </select>
+                         <label htmlFor="pais">País:</label>
+                    </div>
+
+        </form>
+      );
+}
+ 
+export default Formulario;
