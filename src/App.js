f3808986f8eede726b97611868a4431d5dc2d66d
Pablo Velez <pablo.velez.p@gmail.com>
2020-07-11 20:09:59 -0500

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
 
@@ -9,7 +11,8 @@ function App() {
     pais: ''
 })
 const [ consultar, guardarConsultar] =  useState(false)
-
+const [ resultado, guardarResultado] = useState({})
+const [error, guardarError] = useState(false)
 const {ciudad, pais} = busqueda
 
 useEffect(() => {
@@ -19,12 +22,34 @@ useEffect(() => {
     if(consultar) {
       const api =  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=8b37bb7f4053397eb3c79b1c09ef1d9b`)
       const data = await api.json()
-      console.log(data.weather)
+      guardarResultado(data)
+      guardarConsultar(false)
+
+      //detecta si hay resultado
+
+      if(resultado.cod === "404"){
+        guardarError(true)
+      } else {
+        guardarError(false)
+      }
+      
     }
   }
   consultarAPI()
+  // eslint-disable-next-line
 }, [consultar])
 
+
+
+let componente;
+  
+  if(error) {
+    componente =<Error mensaje="No hay resultados" /> 
+  } else {
+    componente = <Clima 
+      resultado={resultado}
+    />
+  }
 
 
   return (
@@ -44,7 +69,7 @@ titulo='Clima react app'
          /> 
         </div>
         <div className="col m6 s12">
-          2
+          <Clima resultado={resultado} />
         </div>
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Header = ({titulo}) => {
     return ( 
@@ -9,6 +10,10 @@ const Header = ({titulo}) => {
         </nav>
 
      );
+}
+
+Header.propTypes = {
+    titulo: PropTypes.string.isRequired
 }
  
 export default Header;
